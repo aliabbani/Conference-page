627ed26d8752bb62e496669b4c5b0604eae1f55b
ali abbani <aliox@ahlam.com>
2021-08-04 11:57:53 +0300

    add mobile menu

---
+++ index.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector('.burger-menu');
+const closeItem = document.querySelector('.close-item');
+const aboutMenu = document.querySelector('.about-menu');
+const programMenu = document.querySelector('.program-menu');
+const speakersMenu = document.querySelector('.speakers-menu');
+const partnersMenu = document.querySelector('.partners-menu');
+const contactMenu = document.querySelector('.contact-menu');
+const conferenceMenu = document.querySelector('.conference-menu');
+
+function hideMenu() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = 'none';
+}
+function showMenu(e) {
+  e.preventDefault();
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display = 'flex';
+}
+
+menu.addEventListener('click', showMenu);
+closeItem.addEventListener('click', hideMenu);
+aboutMenu.addEventListener('click', hideMenu);
+programMenu.addEventListener('click', hideMenu);
+speakersMenu.addEventListener('click', hideMenu); 
+partnersMenu.addEventListener('click', hideMenu);
+contactMenu.addEventListener('click', hideMenu);
+conferenceMenu.addEventListener('click', hideMenu); 
